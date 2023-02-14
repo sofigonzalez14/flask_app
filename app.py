6217955213341f004d5f36a37b037d53fd0bfcbc
sofia gonzalez <sofimacag@gmail.com>
2023-02-14 15:59:11 -0300

    guardado1

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify , request, Response
+from http import HTTPStatus
+import json
+import urllib
+
+app = Flask(__name__)
+if __name__ == "__main__":
+ app.run(debug = True)
+
+with open('\"Archivos_JSON/usuarios.json', encoding='utf-8') as archivo_json:
+    usuarios = json.load(archivo_json)
+@app.route('/')
+def home():
+ return 'SISTEMA DE ENTRADAS: CINE'
+
+@app.route('/')
+def devolver_usuarios():
+    return jsonify(usuarios)
